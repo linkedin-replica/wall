618e1487366e1885ee78b2afbcad964d3dd2e692
YaraA <yaraasy@gmail.com>
2018-02-20 19:52:40 +0200

    arangodb dependency and connect method 🕺

--- src/main/java/database/DatabaseHandler.java
+++ src/main/java/database/DatabaseHandler.java
@@ -1,10 +1,16 @@
 package database;
 
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+
 public interface DatabaseHandler {
+
     /**
      * Initiate a connection with the database
      */
-    void connect();
+     void connect();
 
     /**
      * Close a connection with the database
