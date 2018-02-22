8e108b0c78506ebe30209091d8527ad34aff92e5
khlyy <mohamedkhly@gmail.com>
2018-02-22 17:33:07 +0200

    add bookmark model

--- src/main/java/com/linkedin/replica/wall/models/Bookmark.java
+++ src/main/java/com/linkedin/replica/wall/models/Bookmark.java
@@ -1,4 +1,15 @@
 package com.linkedin.replica.wall.models;
 
 public class Bookmark {
+    static String  postId, userId;
+    /**
+     * bookmark constructor.
+     */
+    public Bookmark(String postId, String userId){
+        this.postId = postId;
+        this.userId = userId;
+    }
+
+  
 }
+
