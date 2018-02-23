4c4d1fa8c5ca1a0a41c882e43b5ef8bd0328f418
khlyy <mohamedkhly@gmail.com>
2018-02-23 19:37:40 +0200

    finish delete bookmark command

--- src/main/java/com/linkedin/replica/wall/commands/impl/DeleteBookmarkCommand.java
+++ src/main/java/com/linkedin/replica/wall/commands/impl/DeleteBookmarkCommand.java
@@ -1,18 +1,31 @@
 package com.linkedin.replica.wall.commands.impl;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 
 import com.linkedin.replica.wall.commands.Command;
+import com.linkedin.replica.wall.models.Bookmark;
 
 public class DeleteBookmarkCommand extends Command{
 
-    public DeleteBookmarkCommand(HashMap<String, String> args) {
-        super(args);
+
+    public DeleteBookmarkCommand(HashMap<String, String> request) throws IOException, ClassNotFoundException {
+        super(request);
     }
 
     public LinkedHashMap<String, Object> execute() {
-        return null;
+        LinkedHashMap<String, Object> response = new LinkedHashMap<String, Object>();
+        String errMsg;
+        if (request.containsKey("userId") && request.containsKey("postId")) {
+            String userId = request.get("userId");
+            String postId = request.get("postId");
+            Bookmark bookmark = new Bookmark(userId, postId);
+            response.put("response", dbHandler.deleteBookmark(bookmark));
+        } else {
+            response.put("response", "missing information");
+        }
+        return response;
     }
 }
 
