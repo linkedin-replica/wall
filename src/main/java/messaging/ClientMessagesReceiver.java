73ecb8e9516b1156a10283e6ff0999084c0b3143
khlyy <mohamedkhly@gmail.com>
2018-03-22 17:22:46 +0200

    bla bla

--- src/main/java/messaging/ClientMessagesReceiver.java
+++ src/main/java/messaging/ClientMessagesReceiver.java
@@ -56,7 +56,7 @@ import java.util.concurrent.TimeoutException;
                     // Extract the request arguments
                     JsonObject object = new JsonParser().parse(new String(body)).getAsJsonObject();
                     String commandName = object.get("commandName").getAsString();
-                    HashMap<String, String> args = new HashMap<>();
+                    HashMap<String, Object> args = new HashMap<>();
                     for(String key: object.keySet())
                         if(!key.equals("commandName"))
                             args.put(key, object.get(key).getAsString());
