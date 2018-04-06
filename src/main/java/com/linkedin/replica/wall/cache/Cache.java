b128d7219d8a9267748c768617bdf1ebd3922926
safa-ads <safa.mohamed.ads@gmail.com>
2018-04-06 19:17:23 +0200

    Cache configurations and connections

---
+++ src/main/java/com/linkedin/replica/wall/cache/Cache.java
@@ -0,0 +1,49 @@
+package com.linkedin.replica.wall.cache;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.Pipeline;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Cache {
+    private JedisPool redis;
+    private  String REDIS_IP;
+    private  int REDIS_PORT;
+    private Properties properties;
+    private static Cache cache;
+
+    private Cache() throws IOException {
+        properties = new Properties();
+        properties.load(new FileInputStream("src/main/resources/cache.config"));
+        REDIS_IP = properties.getProperty("redis.ip");
+        REDIS_PORT = Integer.parseInt(properties.getProperty("redis.port"));
+        redis = new JedisPool(new JedisPoolConfig(),REDIS_IP,REDIS_PORT);
+    }
+
+    public static void init() throws IOException {
+        cache = new Cache();
+    }
+
+    public static Cache getInstance(){
+        return cache;
+    }
+
+    public JedisPool getRedisPool(){
+        return redis;
+    }
+    public void destroyRedisPool(){
+        redis.destroy();
+    }
+
+    public static void main(String[] args) {
+        //Connecting to Redis server on localhost
+        JedisPool jedisPool = new JedisPool(new JedisPoolConfig(),"localhost",6379);
+        Jedis cacheResource = jedisPool.getResource();
+        Pipeline jedisPipeline = cacheResource.pipelined();
+        System.out.println("Server is running: "+jedisPool.getResource().ping());
+    }
+}
