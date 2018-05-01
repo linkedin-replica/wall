e06841b6be5d6cdc6dedf3965d6a78661049d76e
safa-ads <safa.mohamed.ads@gmail.com>
2018-05-01 22:22:56 +0200

    cache added to getPostsCommand

--- src/main/java/com/linkedin/replica/wall/commands/impl/GetPostsCommand.java
+++ src/main/java/com/linkedin/replica/wall/commands/impl/GetPostsCommand.java
@@ -1,10 +1,12 @@
 package com.linkedin.replica.wall.commands.impl;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 
 import com.google.gson.JsonObject;
+import com.linkedin.replica.wall.cache.handlers.PostsCacheHandler;
 import com.linkedin.replica.wall.commands.Command;
 import com.linkedin.replica.wall.database.handlers.DatabaseHandler;
 import com.linkedin.replica.wall.database.handlers.WallHandler;
@@ -19,20 +21,23 @@ public class GetPostsCommand extends Command{
     }
 
 
-    public Object execute() {
+    public Object execute() throws IOException, IllegalAccessException, NoSuchFieldException {
 
         // get database handler that implements functionality of this command
         WallHandler dbHandler = (WallHandler) this.dbHandler;
-
+        //get cache handler
+        PostsCacheHandler postsCacheHandler = (PostsCacheHandler)this.cacheHandler;
         // validate that all required arguments that are passed
         validateArgs(new String[]{"companyId", "limit"});
-
-
         // call dbHandler to list of posts from db
         JsonObject request = (JsonObject) args.get("request");
         String companyId = request.get("companyId").getAsString();
         int limit = request.get("limit").getAsInt();
-        List<ReturnedPost> posts = dbHandler.getPosts(companyId, limit);
+        List<ReturnedPost> posts = (List<ReturnedPost>) postsCacheHandler.getCompanyPosts(companyId,limit,ReturnedPost.class);
+        if( posts == null){
+            posts = dbHandler.getPosts(companyId, limit);
+            postsCacheHandler.cacheCompanyPosts(companyId,posts);
+       }
         return posts;
     }
 
